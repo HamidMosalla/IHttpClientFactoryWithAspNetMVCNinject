b2495a69c04196bff7838a7de250f364089c6137
Hamid Mosalla <Xellarix@gmail.com>
2020-05-22 10:00:12 +0200

    add UrlStringReader service, add package for ihttpclientfactory

---
+++ Services/UrlStringReader.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace IHttpFactoryWithAspNetMVCNinject.Services
+{
+    public class UrlStringReader
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public UrlStringReader(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<string> GetJsonAsyncIncorrectUsage(Uri uri)
+        {
+            using (var client = new HttpClient())
+            {
+                // use ConfigureAwait(false) whenever the rest of that async method does not depend on the current context.
+                // https://stackoverflow.com/a/40220190/1650277
+                return await client.GetStringAsync(uri).ConfigureAwait(false);
+            }
+        }
+
+        public async Task<string> GetJsonAsyncCorrectUsage(Uri uri)
+        {
+            using (var client = _httpClientFactory.CreateClient())
+            {
+                // use ConfigureAwait(false) whenever the rest of that async method does not depend on the current context.
+                // https://stackoverflow.com/a/40220190/1650277
+                return await client.GetStringAsync(uri).ConfigureAwait(false);
+            }
+        }
+    }
+}
